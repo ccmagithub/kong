50156424cfb0161d47063daffb7edcae8af44685
gina <gina.chang@geminiopencloud.com>
2018-05-29 13:57:22 +0800

    feat(acls): defined assign key & group into acls table  or delete key with specific group  (api add CRD functions for using)

--- kong/plugins/acl/api.lua
+++ kong/plugins/acl/api.lua
@@ -1,84 +1,50 @@
 local crud = require "kong.api.crud_helpers"
 
 return {
-  ["/consumers/:username_or_id/acls/"] = {
-    before = function(self, dao_factory, helpers)
-      crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
-      self.params.consumer_id = self.consumer.id
-    end,
-
+  ["/api_key/:key_id/acls/"] = {
+    -- get total group counts & data belongs to this key_id
     GET = function(self, dao_factory)
       crud.paginated_set(self, dao_factory.acls)
     end,
-
-    PUT = function(self, dao_factory)
-      crud.put(self.params, dao_factory.acls)
-    end,
-
+    
+    -- assign groupname into acls with key_id (add row data to acls)
     POST = function(self, dao_factory)
       crud.post(self.params, dao_factory.acls)
     end
   },
 
-  ["/consumers/:username_or_id/acls/:group_or_id"] = {
+  ["/api_key/:key_id/acls/:group"] = {
+    -- convert params and get data in acls table
     before = function(self, dao_factory, helpers)
-      crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
-      self.params.consumer_id = self.consumer.id
-
-      local acls, err = crud.find_by_id_or_field(
-        dao_factory.acls,
-        { consumer_id = self.params.consumer_id },
-        self.params.group_or_id,
-        "group"
-      )
+      self.params.key_id = ngx.unescape_uri(self.params.key_id)
+      self.params.group = ngx.unescape_uri(self.params.group)
+      local row, err = crud.find_by_id_or_field(dao_factory.acls, 
+            { key_id = self.params.key_id , group = self.params.group },
+            self.params.key_id , "key_id")
 
       if err then
         return helpers.yield_error(err)
-      elseif #acls == 0 then
+      elseif #row == 0 then
         return helpers.responses.send_HTTP_NOT_FOUND()
       end
-      self.params.group_or_id = nil
 
-      self.acl = acls[1]
+      self.acl = row[1]
     end,
 
+    -- get acls data by params:key_id & params:group
     GET = function(self, dao_factory, helpers)
       return helpers.responses.send_HTTP_OK(self.acl)
     end,
 
-    PATCH = function(self, dao_factory)
-      crud.patch(self.params, dao_factory.acls, self.acl)
-    end,
-
+    -- delete acls data by params:key_id & params:group
     DELETE = function(self, dao_factory)
-      crud.delete(self.acl, dao_factory.acls)
+        crud.delete(self.acl, dao_factory.acls)
     end
   },
+
   ["/acls"] = {
     GET = function(self, dao_factory)
       crud.paginated_set(self, dao_factory.acls)
-    end
-  },
-  ["/acls/:acl_id/consumer"] = {
-    before = function(self, dao_factory, helpers)
-      local filter_keys = {
-       id = self.params.acl_id
-      }
-
-      local acls, err = dao_factory.acls:find_all(filter_keys)
-      if err then
-        return helpers.yield_error(err)
-      elseif next(acls) == nil then
-        return helpers.responses.send_HTTP_NOT_FOUND()
-      end
-
-      self.params.acl_id = nil
-      self.params.username_or_id = acls[1].consumer_id
-      crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
-    end,
-
-    GET = function(self, dao_factory, helpers)
-      return helpers.responses.send_HTTP_OK(self.consumer)
     end
   }
 }
